b1ed756c7494bbacd32354022b775deb80ce0e5d
mikels0n <myskamichal@gmail.com>
2023-10-18 13:43:57 +0200

    Function that plays one round of RPS added

--- script_RPS.js
+++ script_RPS.js
@@ -2,11 +2,45 @@ function getComputerChoice() {
     let rndNumber = Math.floor(Math.random()*3);
     switch (rndNumber) {
         case 0:
-            return("Rock");
+            return("rock");
         case 1:
-            return("Paper");
+            return("paper");
         case 2:
-            return("Scissors");
+            return("scissors");
     }
 }
 
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+
+function playRound(playerSelection,computerSelection) {
+
+    // Draw
+    if (playerSelection == computerSelection) {
+        return("Draw!")
+    }
+
+    // Player won
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return(playerSelection + " beats " + computerSelection + ", you won!")
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return(playerSelection + " beats " + computerSelection + ", you won!")
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return(playerSelection + " beats " + computerSelection + ", you won!")
+    }
+
+    // Computer won
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return(computerSelection + " beats " + playerSelection + ", you lost!")
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return(computerSelection + " beats " + playerSelection + ", you lost!")
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return(computerSelection + " beats " + playerSelection + ", you lost!")
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection));
